1dcbe0af718023131e26e19c17417fb20836cf47
Nikolaj Sørensen <nikolaj.lund.soerensen@lego.com>
2023-01-08 11:30:05 +0100

    Add CD for terraform IaC to run terraform apply and output Container Id, restart container service

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,6 +6,10 @@ output "keyvaultId" {
   value = module.keyvault.key-vault-id
 }
 
+output "containerResourceId" {
+  value = azurerm_container_group.app.id
+}
+
 output "WebappUrl" {
   value = azurerm_container_group.app.fqdn
 }
